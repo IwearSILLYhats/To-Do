f3869b2e505470e3d47732cd8851237ec0f82b72
Austin <austin.moorer@gmail.com>
2023-02-05 15:48:32 -0500

    Started work on function to generate main content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { formatDistanceToNow, format, add, getMonth, getDate, getYear } from "date-fns";
+import { formatDistanceToNow, format, add, getMonth, getDate, getYear, getHours, getMinutes } from "date-fns";
 
 // manages any events on the page that requires a class to be toggled
 (function toggleEvents(){
@@ -21,14 +21,14 @@ import { formatDistanceToNow, format, add, getMonth, getDate, getYear } from "da
     });
     newBtn.addEventListener('click', (e) => {
         classToggle(eventForm, 'hidden');
+        eventForm.querySelector("#name").focus();
     });
 })();
 
 // constructor for event list items, days need to have one added due to how they are indexed when converting from date input to Date objects
 class ListItem {
-    constructor(name, dates, time, notes){
+    constructor(name, dates, notes){
         this.name = name;
-        console.log(dates);
         this.date = dates ? new Date (dates) : 'Anytime';
         this.notes = notes;
         this.subItem = [];
@@ -36,13 +36,25 @@ class ListItem {
     }
     get getMDY() {
             let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-        return `${months[getMonth(this.date)]} ${getDate(this.date)}, ${getYear(this.date)}`; }
+            return `${months[getMonth(this.date)]} ${getDate(this.date)}, ${getYear(this.date)}`; }
+    get getHoursMins () {
+        let hours = getHours(this.date);
+        let minutes = getMinutes(this.date);
+        let ampm = 'am';
+        if(hours > 12){
+            hours -= 12;
+            ampm = 'pm';
+        }
+        hours = (hours.length === 2) ? hours : `0${hours}`;
+        minutes = (minutes.length === 2) ? minutes : `0${minutes}`;
+        return hours +":" + minutes + ampm;
+    }
 }
 
 // houses the event library object and any functions manipulating it.
 (function eventLibrary (){
-    let library = [new ListItem ('TestName', new Date(1969, 7, 9), 'No notes'),
-     new ListItem ('OtherTestName', new Date(2002, 11, 12), 'No Notes')];
+    let library = [new ListItem ('TestName', new Date(1969, 7, 9), ''),
+     new ListItem ('OtherTestName', new Date(2002, 11, 12), 'Some notes and maybe even some stupid thoughts on how this friggin app should proceed!')];
     
     const submitBtn = document.querySelector('#submitBtn');
     const eventForm = document.querySelector('.newEvent');
@@ -75,14 +87,24 @@ function buildNav (library){
         const child = document.createElement('li');
         const nameField = document.createElement('p');
         nameField.textContent = eventItem.name;
+        child.addEventListener('click', (e) => {
+            buildContent(eventItem);
+        })
         child.appendChild(nameField);
         if (eventItem.date !== 'Anytime'){
             const dateField = document.createElement('p');
-            dateField.textContent = eventItem.getMDY;
+            if(parent === categories[0]){
+                dateField.textContent = formatDistanceToNow(eventItem.date);
+            }
+            else{
+                dateField.textContent = eventItem.getMDY;
+            }
             child.appendChild(dateField);
         }
         parent.appendChild(child);
     }
+
+    // Checks event date vs todays date to determine which navbar category it should go into.
         library.forEach((newItem) => {
             const msToDays = 1000*60*60*24;
             let dayDifference = (new Date(newItem.date) - Date.now())/msToDays;
@@ -92,7 +114,22 @@ function buildNav (library){
                         : dayDifference <= 7 ? 1
                         : dayDifference <= 30 ? 2
                         : 3;
-            console.log(dayDifference);
             addChild(categories[category], newItem);
         });
+}
+// Clears main content element, then dynamically generates new elements based on the content of eventItem that was clicked
+function buildContent (eventItem){
+    const main = document.querySelector('.main');
+    while (main.firstChild){
+        main.removeChild(main.firstChild);
+    }
+    function elementBuilder(parent, data, element){
+        const child = document.createElement(element);
+        child.textContent = data;
+        parent.appendChild(child);
+    }
+    elementBuilder(main, eventItem.name, 'h1');
+    elementBuilder(main, `${eventItem.getMDY} ${eventItem.getHoursMins}`, 'h3');
+    elementBuilder(main, eventItem.notes, 'p');
+
 }
