533cc67efe6ae3624e7ef0a4f23a3996824952b7
Austin <austin.moorer@gmail.com>
2023-03-20 21:30:27 -0400

    Added CRUD to event library and done/remove

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ import { formatDistanceToNow, format, add, getMonth, getDate, getYear, getHours,
     const page = document.querySelector('html');
     const newBtn = document.querySelector('.new');
     const eventForm = document.querySelector('.newEvent');
-
+    const options = document.querySelector('.options');
+    const optionBtn = document.querySelector('.optionBtn');
     function classToggle(target, tglClass, state){
         target.classList.toggle(tglClass, state);
     }
 
-    darkModeToggle.addEventListener('change', (e) => { 
+
+    if (localStorage.getItem('darkMode') == 'true'){
+        page.classList.add('dark');
+        darkModeToggle.checked = true;
+    }
+    darkModeToggle.addEventListener('change', (e) => {
         classToggle(page, 'dark', darkModeToggle.checked);
+        localStorage.setItem('darkMode', darkModeToggle.checked);
     });
     document.addEventListener('click', (e) => {
         if(!e.target.closest('.newEvent') && !e.target.closest('.new')){
@@ -23,17 +30,26 @@ import { formatDistanceToNow, format, add, getMonth, getDate, getYear, getHours,
         classToggle(eventForm, 'hidden');
         eventForm.querySelector("#name").focus();
     });
+    document.addEventListener('click', (e) => {
+        if(!e.target.closest('.options') && !e.target.closest('.optionBtn')){
+            classToggle(options, 'hidden', true);
+        };
+    });
+    optionBtn.addEventListener('click', (e) => {
+        classToggle(options, 'hidden');
+    });
 })();
 
 const eventLibrary = (function(){
         // constructor for event list items, days need to have one added due to how they are indexed when converting from date input to Date objects
         class ListItem {
-            constructor(name, dates, notes, subItem, category){
+            constructor(name, dates, notes, subItem, category, done){
                 this.name = name;
                 this.date = dates ? new Date (dates): 'Anytime';
                 this.notes = notes;
                 this.subItem = subItem ?? [];
                 this.category = category ?? [];
+                this.done = done ?? false;
             }
             get formatDate (){
                 return `${this.getMDY} ${this.getHoursMins}`;
@@ -54,11 +70,25 @@ const eventLibrary = (function(){
                 return hours +":" + minutes + ampm;
             }
             updateData (input, field, index){ 
-                if (field === 'subItem') {this[field][index] = input;}
+                if (field === 'subItem') {this[field][index].val = input;}
                 else{this[field] = input;}
                 if (field === 'name' || field === 'date'){
                     buildNav(library);
                 }}
+            remove (){
+                const index = library.indexOf(this);
+                if (index !== -1){
+                    library.splice(index, 1);
+                    buildNav(library);
+                    buildContent();
+                    updateStorage();
+                }
+            }
+            toggleDone (){
+                this.done = (this.done === false) ? true : false;
+                buildNav(library);
+                updateStorage();
+            }
         }
 
     //Data object that holds event data, feeds to and from localstorage
@@ -67,7 +97,7 @@ const eventLibrary = (function(){
         if(localStorage.getItem('library')){
             let temp = JSON.parse(localStorage.getItem('library'));
             library = temp.map((e) => {
-            return new ListItem(e.name, Date.parse(e.date), e.notes, e.subItem, e.category);
+            return new ListItem(e.name, Date.parse(e.date), e.notes, e.subItem, e.category, e.done);
         });
         };
 
@@ -103,7 +133,7 @@ const eventLibrary = (function(){
 
 // Clears existing li dom elements then generates new ones that correspond to library items, categorized by time between current date and event date
 function buildNav (library){
-    const categories = [...document.querySelectorAll('ul')];
+    const categories = [...document.querySelectorAll('.nav ul')];
     categories.forEach( element => removeChildren(element));
     function removeChildren(parent){
         while (parent.firstChild){
@@ -114,12 +144,18 @@ function buildNav (library){
         const child = document.createElement('li');
         const nameField = document.createElement('p');
         nameField.textContent = eventItem.name;
+        if(eventItem.done === true){
+            nameField.classList.add('done');
+        };
         child.addEventListener('click', (e) => {
             buildContent(eventItem);
         })
         child.appendChild(nameField);
         if (eventItem.date !== 'Anytime'){
             const dateField = document.createElement('p');
+            if (eventItem.done === true){
+                dateField.classList.add('done');
+            };
             if(parent === categories[0]){
                 dateField.textContent = formatDistanceToNow(eventItem.date);
             }
@@ -135,8 +171,8 @@ function buildNav (library){
         library.forEach((newItem) => {
             const msToDays = 1000*60*60*24;
             let dayDifference = (new Date(newItem.date) - Date.now())/msToDays;
-            let category = newItem.date === 'Anytime' ? 4
-                        : dayDifference < 0 ? 5
+            let category = (dayDifference < 0 || newItem.done === true) ? 5
+                        : newItem.date === 'Anytime' ? 4
                         : dayDifference <= 1 ? 0
                         : dayDifference <= 7 ? 1
                         : dayDifference <= 30 ? 2
@@ -147,68 +183,130 @@ function buildNav (library){
 // Clears main content element, then dynamically generates new elements based on the content of eventItem that was clicked
 function buildContent (eventItem){
     const main = document.querySelector('.main');
-    const components = [
-        [main, eventItem.name, 'h1', 'name'], 
-        [main, eventItem.date === 'Anytime' ? eventItem.date : eventItem.formatDate, 'h3', 'date'], 
-        [main, eventItem.notes, 'p', 'notes'], 
-        [main, eventItem.subItem, 'ul', 'subItem']];
-
     while (main.firstChild){
         main.removeChild(main.firstChild);
     }
-    function elementBuilder(parent, data, element, types, index){
-        const label = document.createElement('label');
-        const child = document.createElement(element);
-        // Checks if data is an array, if so runs elementBuilder again on array indexes, if not adds event listener so that changes to input change the text element
+    if(eventItem){
+    const components = [
+        [main, eventItem.name, 'name'], 
+        [main, eventItem.date === 'Anytime' ? eventItem.date : eventItem.formatDate, 'date'], 
+        [main, eventItem.notes, 'notes'], 
+        [main, eventItem.subItem, 'subItem']];
+    function elementBuilder(parent, data, types, index){
+        // Checks if data is an array, if so runs itself recursively on array indexes, if not adds event listener so that changes to input change the value in library
         if(Array.isArray(data)){
-            data.forEach((point, index) => {
-                elementBuilder(child, point, 'li', 'subItem', index);
+            const child = document.createElement('ul');
+            data.forEach((lineItem, indx) => {
+                elementBuilder(child, lineItem, 'subItem', indx);
             });
+            parent.appendChild(child);
         }
         else{
-            const fieldType = types === 'date' ? ['input', 'datetime-local'] :
-            types === 'notes' ? ['textarea'] :
-                ['input', 'text'];
-            const editField = document.createElement(fieldType[0]);
-            if(fieldType[1]){
-                editField.setAttribute('type', fieldType[1])
-            };
+            const container = document.createElement('div');
+            let editField;
+            if (types === 'notes'){
+                editField = document.createElement('textarea');
+                editField.setAttribute('maxlength', '140');
+                editField.value = data === '' ? 'New Item' : data.val ?? data;
+            }
+            else{
+                editField = document.createElement('input');
+                if (types === 'date'){
+                    editField.setAttribute('type', 'datetime-local');
+                    if (eventItem.date !== 'Anytime'){
+                        const localTime = new Date(eventItem.date.getTime() + eventItem.date.getTimezoneOffset() * -60 * 1000).toISOString().slice(0, 19);
+                        editField.value = localTime;
+                    }
+                }
+                else{
+                    editField.setAttribute('type', 'text');
+                    editField.setAttribute('maxlength', '50')
+                    editField.value = data.val === '' ? 'New Item' : data.val ?? data;
+                    if (data.done === true){editField.classList.add('done')};
+                }
+            }
             editField.addEventListener('change', (e) => {
                 if(types === 'date'){
-                    eventItem.updateData((data ? new Date (e.target.value): 'Anytime'), types);
-                    child.textContent = eventItem.date === 'Anytime' ? 'Anytime' : eventItem.formatDate;
+                    eventItem.updateData((e.target.value ? new Date (e.target.value) : 'Anytime'), types);
                 }
                 else if(types === 'subItem') {
                     eventItem.updateData(e.target.value, types, index);
-                    child.textContent = e.target.value;
-
                 }
                 else{
                     eventItem.updateData(e.target.value, types)
-                    child.textContent = e.target.value;
                 }
                 eventLibrary.updateLocal();
+            });
+            container.addEventListener('click', (e) => {
+                if (e.target.matches('.main ul div::before')){
+
+                };
+                if (e.target.matches('.main ul div::after')){
+                }
             })
-            editField.value = data == '' ? 'New Item' : data;
-            child.textContent = data == '' ? 'New Item' : data;
-            label.appendChild(editField);
+            if(types === 'subItem'){
+                const before = document.createElement('button');
+                before.type = 'button';
+                before.textContent = '✔';
+                before.classList.add('before');
+                before.setAttribute('title', 'Complete');
+                before.addEventListener('click', (e) => {
+                    editField.classList.toggle('done');
+                    data.done = (data.done === true) ? false : true;
+                });
+                container.appendChild(before);
+            }
+            container.appendChild(editField);
+            if(types === 'subItem'){
+                const after = document.createElement('button');
+                after.type = 'button';
+                after.textContent = '✕';
+                after.classList.add('after');
+                after.setAttribute('title', 'Remove');
+                after.addEventListener('click', (e) => {
+                    eventItem.subItem.splice(index, 1);
+                    buildContent(eventItem);
+                });
+                container.appendChild(after);
+            }
+            parent.appendChild(container);
         }
-        label.appendChild(child);
-        parent.appendChild(label);
-        return child;
     }
 
  components.forEach((item) => {
     elementBuilder(...item);
  });
+
  const addBtn = document.createElement('button');
  addBtn.type = 'button';
- addBtn.textContent = 'Add Sub-item';
+ addBtn.classList.add('addBtn');
+ addBtn.textContent = '+';
+ addBtn.setAttribute('title', 'New Item');
  addBtn.addEventListener('click', (e) => {
-     eventItem.subItem.push('');
+     eventItem.subItem.push({val: '', done: false});
      const subItemList = document.querySelector('.main ul');
-     elementBuilder(subItemList, eventItem.subItem[eventItem.subItem.length-1], 'li', `subItem`, eventItem.subItem.length-1);
-     
+     elementBuilder(subItemList, eventItem.subItem[eventItem.subItem.length-1], `subItem`, eventItem.subItem.length-1);
  });
  main.appendChild(addBtn);
-}
+
+  const doneBtn = document.createElement('button');
+  doneBtn.type = 'button';
+  doneBtn.classList.add('doneBtn');
+  doneBtn.textContent = '✔';
+  doneBtn.setAttribute('title', 'Complete');
+  doneBtn.addEventListener('click', (e) => {
+    eventItem.toggleDone();
+  });
+  main.appendChild(doneBtn);
+
+  const removeBtn = document.createElement('button');
+  removeBtn.type = 'button';
+  removeBtn.classList.add('removeBtn');
+  removeBtn.textContent = '✕';
+  removeBtn.setAttribute('title', 'Remove');
+  removeBtn.addEventListener('click', (e) => {
+    eventItem.remove();
+  });
+  main.appendChild(removeBtn);
+};
+};
